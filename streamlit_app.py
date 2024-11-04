b3867d6c3cbb4480dc68e5c00ef16ca27afebf97
kajtorngren <115929198+kajtorngren@users.noreply.github.com>
2024-11-04 22:59:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,47 +1,7 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 import altair as alt
-
-# Custom CSS for a black background and white text
-st.markdown(
-    """
-    <style>
-        /* Set the entire app background to black */
-        .main {
-            background-color: #000000;
-            color: #FFFFFF;
-        }
-
-        /* Change sidebar background color to black */
-        section[data-testid="stSidebar"] {
-            background-color: #333333;
-        }
-        
-        /* Adjust text color in sidebar */
-        section[data-testid="stSidebar"] .stTextInput, 
-        section[data-testid="stSidebar"] .stSlider {
-            color: #FFFFFF;
-        }
-
-        /* Set white color for expander content */
-        .st-expander {
-            color: #FFFFFF;
-        }
-
-        /* Set DataFrame and other text colors to white */
-        .stTable, .stDataFrame {
-            color: #FFFFFF;
-            background-color: #000000;
-        }
-        
-        /* Make the headers and titles white */
-        .stMarkdown h1, .stMarkdown h2, .stMarkdown h3, .stMarkdown h4, .stMarkdown h5, .stMarkdown h6 {
-            color: #FFFFFF;
-        }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
 
 st.title('🫀 Heart Disease ML Diagnosis 🫀')
 
