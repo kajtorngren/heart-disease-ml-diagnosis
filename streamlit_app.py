b4822305e7ca5b0d652c1245a226095088bafe76
kajtorngren <115929198+kajtorngren@users.noreply.github.com>
2024-11-05 02:22:27 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import numpy as np
 import altair as alt
 
 # Add the banner image
-st.image("cardiology.jpg", height=300)  # Sets the height to 300 pixels
+st.image("cardiology.jpg", use_column_width=True) 
 
 st.title('🫀 Heart Disease ML Diagnosis 🫀')
 
