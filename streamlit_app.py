b43e8f3678f77d7280a33a149a58526438486b57
kajtorngren <115929198+kajtorngren@users.noreply.github.com>
2024-12-12 22:01:29 +0100

    login/logout

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,7 +79,7 @@ if choice == 'Sign up':
 
 # Login Block
 if choice == 'Login':
-    login = st.sidebar.checkbox('Login')
+    login = st.sidebar.checkbox('Login/Logout')
     if login:
         user = auth.sign_in_with_email_and_password(email,password)
         st.sidebar.success('Login successful!')
