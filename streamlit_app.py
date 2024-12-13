11665fe027063eb4fa0f379548011801a91d53f3
kajtorngren <115929198+kajtorngren@users.noreply.github.com>
2024-12-13 11:44:13 +0100

    welcome

--- streamlit_app.py
+++ streamlit_app.py
@@ -83,7 +83,10 @@ if choice == 'Login':
     if login:
         user = auth.sign_in_with_email_and_password(email,password)
         st.sidebar.success('Login successful!')
-        st.sidebar.title(f'Welcome {handle}')
+        user = auth.sign_in_with_email_and_password(email, password)
+        db.child(user['localId']).child("Handle").set(handle)
+        db.child(user['localId']).child("ID").set(user['localId'])
+        st.title(f'Welcome {handle}')
         st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
         bio = st.sidebar.radio('Jump to',['Input features','Heart disease prediction', 'History'])
     
