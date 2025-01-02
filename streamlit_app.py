cffab1e7b61e79b1f416b914ff8516543aa8c37f
kajtorngren <115929198+kajtorngren@users.noreply.github.com>
2025-01-02 16:53:18 +0100

    pink

--- streamlit_app.py
+++ streamlit_app.py
@@ -361,9 +361,9 @@ if choice == 'Login':
                 
                 st.success('Post uploaded!!')
 
-            st.header(' :violet[History] ')
+            st.header(' :pink[History] ')
 
-            
+
             docs = db.collection('Posts').get()
             
             for doc in docs:
