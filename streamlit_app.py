698426773b409fdee4904c6771eeff6bb44904e8
kajtorngren <115929198+kajtorngren@users.noreply.github.com>
2025-01-02 20:52:49 +0100

    username

--- streamlit_app.py
+++ streamlit_app.py
@@ -339,25 +339,32 @@ if choice == 'Login':
             #History and Posts
 
             post = st.text_input("Share your current mood, inputs and results!",max_chars = 200)
+            user = auth.get_user_by_email(email)
+            print(user.uid)
+            st.session_state.username = user.uid
+            st.session_state.useremail = user.email
+            
+            global Usernm
+            Usernm=(user.uid)
     
             if st.button('Post'):
                 if post!='':
-                    info = db.collection('Posts').document(localId).get()
+                    info = db.collection('Posts').document(st.session_state.username).get()
 
                 if info.exists:
                     info = info.to_dict()
 
                     if 'Content' in info.keys():
-                        pos=db.collection('Posts').document(localId)
+                        pos=db.collection('Posts').document(st.session_state.username)
                         pos.update({u'Content': firestore.ArrayUnion([u'{}'.format(post)])})
                         # st.write('Post uploaded!!')
                     else:
-                        data={"Content":[post],'Username':localId}
-                        db.collection('Posts').document(localId).set(data)    
+                        data={"Content":[post],'Username':st.session_state.username}
+                        db.collection('Posts').document(st.session_state.username).set(data)    
                 else:
                     
-                    data={"Content":[post],'Username':localId}
-                    db.collection('Posts').document(localId).set(data)
+                    data={"Content":[post],'Username':st.session_state.username}
+                    db.collection('Posts').document(st.session_state.username).set(data)
                 
                 st.success('Post uploaded!!')
 
