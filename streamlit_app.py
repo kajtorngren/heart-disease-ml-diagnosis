92135d6c33c5607728871e3b105ba90406abfe76
kajtorngren <115929198+kajtorngren@users.noreply.github.com>
2025-01-02 16:44:55 +0100

    uid

--- streamlit_app.py
+++ streamlit_app.py
@@ -338,8 +338,7 @@ if choice == 'Login':
             
 
             post = st.text_input("Share your current mood, inputs and results!",max_chars = 200)
-            user = auth.get_user_by_email(email)
-            st.session_state.username = user.uid
+            uid = user['localId']
     
             if st.button('Post'):
                 if post!='':
@@ -349,16 +348,16 @@ if choice == 'Login':
                     info = info.to_dict()
 
                     if 'Content' in info.keys():
-                        pos=db.collection('Posts').document(user.uid)
+                        pos=db.collection('Posts').document(uid)
                         pos.update({u'Content': firestore.ArrayUnion([u'{}'.format(post)])})
                         # st.write('Post uploaded!!')
                     else:
-                        data={"Content":[post],'Username':user.uid}
-                        db.collection('Posts').document(user.uid).set(data)    
+                        data={"Content":[post],'Username':uid}
+                        db.collection('Posts').document(uid).set(data)    
                 else:
                     
-                    data={"Content":[post],'Username':user.uid}
-                    db.collection('Posts').document(user.uid).set(data)
+                    data={"Content":[post],'Username':uid}
+                    db.collection('Posts').document(uid).set(data)
                 
                 st.success('Post uploaded!!')
 
