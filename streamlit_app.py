0988f29d670f411b70e4e1001b43d8666cd8876c
kajtorngren <115929198+kajtorngren@users.noreply.github.com>
2025-01-02 18:20:28 +0100

    back

--- streamlit_app.py
+++ streamlit_app.py
@@ -361,16 +361,16 @@ if choice == 'Login':
                 
                 st.success('Post uploaded!!')
 
-        st.header(' :violet[History] ')
+            st.header(' :violet[History] ')
 
             
-        docs = db.collection('Posts').get()
+            docs = db.collection('Posts').get()
             
-        for doc in docs:
-            d=doc.to_dict()
-            try:
-                st.text_area(label=':green[Posted by:] '+':orange[{}]'.format(d['Username']),value=d['Content'][-1],height=20)
-            except: pass
+            for doc in docs:
+                d=doc.to_dict()
+                try:
+                    st.text_area(label=':green[Posted by:] '+':orange[{}]'.format(d['Username']),value=d['Content'][-1],height=20)
+                except: pass
 
 
         except Exception as e:
