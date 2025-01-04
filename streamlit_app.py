c6b1efe064e49aeed85cef7121319f82377a38c7
kajtorngren <115929198+kajtorngren@users.noreply.github.com>
2025-01-04 01:12:23 +0100

    tst

--- streamlit_app.py
+++ streamlit_app.py
@@ -407,11 +407,14 @@ if choice == 'Login':
             if st.button('Save'):
                 user_data = input_df.to_dict(orient='records')[0]  # Convert input data to dictionary
                 
+                timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
                 # Save to Firestore under the "Users" collection, keyed by user ID
                 user_id = user['localId']  # Replace with dynamic user ID, e.g., from authentication
                 post_data = {
                     'UserID': user_id,
                     'UserInput': user_data
+                    'Timestamp': timestamp  # Add the timestamp to user inputs
                 }
 
                 # Add or update user data in Firestore
