b34f96718161ccbe2bf572c1c235669cf1edc92b
erik99holm <77684216+erik99holm@users.noreply.github.com>
2025-01-06 11:23:54 +0100

    redo BP info

--- streamlit_app.py
+++ streamlit_app.py
@@ -178,7 +178,8 @@ if choice == 'Login':
 
                 st.sidebar.info(
                 """
-                **Resting Blood Pressure**: This is the **systolic blood pressure** (top/first number) measured in mm Hg.  
+                **Resting Blood Pressure**:
+                This is the **systolic blood pressure** (top/first number) measured in mm Hg.  
                 """
                 )
 
