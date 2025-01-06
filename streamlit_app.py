b82f63cf9eb4a55ef219beb8d9fab421f6be2466
kajtorngren <115929198+kajtorngren@users.noreply.github.com>
2025-01-07 00:01:54 +0100

    back

--- streamlit_app.py
+++ streamlit_app.py
@@ -88,7 +88,7 @@ if choice == 'Login':
         try:
             # Authenticate user
             user = auth.sign_in_with_email_and_password(email, password)
-            st.sidebar.success(f'Login successful! Welcome {handle}')
+            st.sidebar.success('Login successful!')
             st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
 
 #######################################################################################
