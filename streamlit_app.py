e5da6e4c08cc018767924694303e0c687c8f0e4b
kajtorngren <115929198+kajtorngren@users.noreply.github.com>
2025-01-07 22:44:22 +0100

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -478,7 +478,7 @@ if choice == 'Login':
                             'Resting blood pressure': user_input.get('trestbps', ''),
                             'Cholesterol': user_input.get('chol', ''),
                             'Max heart rate': user_input.get('thalach', ''),
-                            'Total Prediction': entry.get('TotalPrediction')  # Corrected here
+                            'Total Prediction': entry('TotalPrediction')  # Corrected here
                         }
                         table_data.append(row)
 
