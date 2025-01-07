b1bf2ef0a6d7e8326686df219feb80a99a22bacd
kajtorngren <115929198+kajtorngren@users.noreply.github.com>
2025-01-07 22:45:39 +0100

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -435,7 +435,7 @@ if choice == 'Login':
                         'Timestamp': current_time,
                         'MoodPost': post,
                         'UserInput': user_data,
-                        'TotalPrediction':
+                        'TotalPrediction': res[0]
                     }
 
                     # Save or update the data in Firestore under the "UserData" collection
