a506d7d5a887db77e375678e9ad8373f8087c42b
erik99holm <77684216+erik99holm@users.noreply.github.com>
2025-01-11 16:30:05 +0100

    text change

--- streamlit_app.py
+++ streamlit_app.py
@@ -363,10 +363,10 @@ if choice == 'Login':
                     res, image = run_ensemble(percentage_ones,prediction[0],BPCh_pred_prob)
 
                     if len(res) > 1:
-                        st.subheader(f'{res[0]} {res[1]}')
+                        st.text(f'{res[0]} {res[1]}')
                     else:
                         st.image(image,caption="",width=350)
-                        st.subheader(res[0])
+                        st.text(res[0])
                     
                     st.session_state.res = res  # Store the result in session state
 
